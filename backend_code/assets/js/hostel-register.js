bf1a9675ca8a7d159a1024806b4fe3a1ea750d34
devjypadyan <devjypadyan@gmail.com>
2024-08-09 00:58:12 +0530

    added image upload and dynamic form functions

--- backend_code/assets/js/hostel-register.js
+++ backend_code/assets/js/hostel-register.js
@@ -7,7 +7,52 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase();
 const storage = getStorage(app);
 
-let hostelimg;
+var files = [];
+let imagelink = [];
+document.getElementById("files").addEventListener("change", function (e) {
+  files = e.target.files;
+  for (let i = 0; i < files.length; i++) {
+  }
+});
+
+document.getElementById("uploadImage").addEventListener("click", async function () {
+
+  var hname = document.getElementById("hostelname").value;
+  var htype = document.getElementById("hosteltype").value;
+  var hphone = document.getElementById("hostelphone").value;
+  var hemail = document.getElementById("hostelemail").value;
+  var hadd1 = document.getElementById("hosteladd1").value;
+  var hadd2 = document.getElementById("hosteladd2").value;
+  var hcity = document.getElementById("hostelcity").value;
+  var hstate = document.getElementById("hostelstate").value;
+  var hpin = document.getElementById("hostelpin").value;
+  var hrent = document.getElementById("roomprice").value;
+  var hfood = document.getElementById("foodprice").value;
+  var ac = document.getElementById("acprice").value;
+  var nonac = document.getElementById("nonprice").value;
+  //checks if files are selected
+  if (files.length != 0) {
+    //Loops through all the selected files
+    for (let i = 0; i < files.length; i++) {
+      const storageRef = ref2(storage, 'images/' + hname + '/hostelImg/' + files[i].name);
+      const upload = await uploadBytes(storageRef, files[i]);
+      const imageUrl = await getDownloadURL(storageRef);
+      imagelink.push(imageUrl);
+    }
+
+    const imageRef = ref(db, 'Hostel details/' + hname + '/ImageData/' + '/');
+    set(imageRef, imagelink)
+      .then(() => {
+        console.log('Image URLs have been successfully stored!');
+      })
+
+  } else {
+    alert("No file chosen");
+  }
+});
+
+/*Single image upload*/
+/*let hostelimg;
 uploadImage.addEventListener('click', (e) => {
 
     upload();
@@ -30,53 +75,81 @@ async function upload() {
         console.log(hostelimg);
         //image.append(imageURL);
     }
-}
+}*/
+addroom.addEventListener('click', (e) => {
+  var hname = document.getElementById("hostelname").value;
+  var htype = document.getElementById("hosteltype").value;
+  var hphone = document.getElementById("hostelphone").value;
+  var hemail = document.getElementById("hostelemail").value;
+  var hadd1 = document.getElementById("hosteladd1").value;
+  var hadd2 = document.getElementById("hosteladd2").value;
+  var hcity = document.getElementById("hostelcity").value;
+  var hstate = document.getElementById("hostelstate").value;
+  var hpin = document.getElementById("hostelpin").value;
+  var hrent = document.getElementById("roomprice").value;
+  var hfood = document.getElementById("foodprice").value;
+  var ac = document.getElementById("acprice").value;
+  var nonac = document.getElementById("nonprice").value;
+
+  var roomtype = document.getElementById("rtype").value;
+  var roomno = document.getElementById("rnumber").value;
+  var roomprice = document.getElementById("rprice").value;
+  var roomfac = document.getElementById("rfac").value;
+
+  set(ref(db, 'Hostel details/' + hname + '/Room details/' + roomtype), {
+
+    Roomtype: roomtype,
+    Roomno: roomno,
+    Roomprice: roomprice,
+    Roomfac: roomfac
+
+  })
+  alert("Room details added successfully");
+
+});
 
 registerHostel.addEventListener('click', (e) => {
+  var hname = document.getElementById("hostelname").value;
+  var htype = document.getElementById("hosteltype").value;
+  var hphone = document.getElementById("hostelphone").value;
+  var hemail = document.getElementById("hostelemail").value;
+  var hadd1 = document.getElementById("hosteladd1").value;
+  var hadd2 = document.getElementById("hosteladd2").value;
+  var hcity = document.getElementById("hostelcity").value;
+  var hstate = document.getElementById("hostelstate").value;
+  var hpin = document.getElementById("hostelpin").value;
+  var hrent = document.getElementById("roomprice").value;
+  var hfood = document.getElementById("foodprice").value;
+  var ac = document.getElementById("acprice").value;
+  var nonac = document.getElementById("nonprice").value;
 
-    var hname = document.getElementById("hostelname").value;
-    var htype = document.getElementById("hosteltype").value;
-    var hphone = document.getElementById("hostelphone").value;
-    var hemail = document.getElementById("hostelemail").value;
-    var hadd1 = document.getElementById("hosteladd1").value;
-    var hadd2 = document.getElementById("hosteladd2").value;
-    var hcity = document.getElementById("hostelcity").value;
-    var hstate = document.getElementById("hostelstate").value;
-    var hpin = document.getElementById("hostelpin").value;
-    var hrent = document.getElementById("roomprice").value;
-    var hfood = document.getElementById("foodprice").value;
-    var ac = document.getElementById("acprice").value;
-    var nonac = document.getElementById("nonprice").value;
-    var imgurl = sessionStorage.getItem("hostelurl");
+  var roomtype = document.getElementById("rtype").value;
+  var roomno = document.getElementById("rnumber").value;
+  var roomprice = document.getElementById("rprice").value;
+  var roomfac = document.getElementById("rfac").value;
 
-    if (hname == null) {
+  update(ref(db, "Hostel details/" + hname + '/'), {
+    Hostelname: hname,
+    Hosteltype: htype,
+    Hostelphone: hphone,
+    Hostelemail: hemail,
+    Hosteladd1: hadd1,
+    Hosteladd2: hadd2,
+    Hostelcity: hcity,
+    Hostelstate: hstate,
+    Hostelpin: hpin,
+    Hostelrent: hrent,
+    Hostelfood: hfood,
+    Acprice: ac,
+    Nonacprice: nonac,
 
-        alert("Please enter hostel name to proceed");
-
-    }
-
-    set(ref(db, "Hostel details/" + hname + '/'), {
-        Hostelname: hname,
-        Hosteltype: htype,
-        Hostelphone: hphone,
-        Hostelemail: hemail,
-        Hosteladd1: hadd1,
-        Hosteladd2: hadd2,
-        Hostelcity: hcity,
-        Hostelstate: hstate,
-        Hostelpin: hpin,
-        Hostelrent: hrent,
-        Hostelfood: hfood,
-        Acprice: ac,
-        Nonacprice: nonac,
-        Imagelink: imgurl
+  })
+    .then(() => {
+      window.location.href = "././products.html";
     })
-        .then(() => {
-            window.location.href = "././products.html";
-        })
-        .catch((error) => {
-            alert(error);
-        });
+    .catch((error) => {
+      alert(error);
+    });
 
 });
 
